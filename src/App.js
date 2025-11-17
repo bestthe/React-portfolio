5c83c6627b1273ca0adedba7a365cf84cb6a1a8d
HS <anjdal4849@naver.com>
2025-11-17 15:54:06 +0900

    v1.2 Main 레이아웃 및 스타일 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header';
+import Main from './components/main';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
