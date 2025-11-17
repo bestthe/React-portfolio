aac20035c00505e6c6f8f539249aa62b3ddb9c1f
HS <anjdal4849@naver.com>
2025-11-17 16:29:51 +0900

    v1.2.2 Main 컴포넌트 구조 및 스타일 최적화

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import '../styles/main.css';
 
 function Main() {
+  const lines = ['lt', 'lb', 'rt', 'rb'];
+
   return (
     <section id="main">
       <div className="main_wrap">
-        <div className="main_line lt"></div>
-        <div className="main_line lb"></div>
-        <div className="main_line rt"></div>
-        <div className="main_line rb"></div>
+        {lines.map((dir) => (
+          <div key={dir} className={`main_line ${dir}`}></div>
+        ))}
         <div className="main_textbox">
           <h1>
-          Web Publisher
-          <br />
-          Portfolio
+            Web Publisher
+            <br />
+            Portfolio
           </h1>
           <p className="subtext">
             안녕하세요. 1년 7개월 차 웹퍼블리셔 박현수입니다.
