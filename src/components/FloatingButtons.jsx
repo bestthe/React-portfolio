e5f5681e9aa59683e9775502bb681edbe8aa20d8
HS <anjdal4849@naver.com>
2025-11-28 17:31:54 +0900

    v1.18 FloatingButtons 버튼 전환 애니메이션 개선 및 최적화

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import { ChatFill, ArrowUp } from 'react-bootstrap-icons';
+import React, { useState } from 'react';
+import { ChatFill, ArrowUp, X } from 'react-bootstrap-icons';
 import '../styles/FloatingButtons.css';
+import ChatWindow from '../components/chatwindow';
 
 function FloatingButtons() {
+  const [isChat, setIsChat] = useState(true);
+
+  const toggleChat = () => setIsChat((prev) => !prev);
+
   const scrollTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
-    <div id="floatingButtons">
-      <button type="button" className="floatingBtn topBtn" onClick={scrollTop}>
-        <ArrowUp size={25} color="#fff" />
-      </button>
-      <button type="button" className="floatingBtn chatBtn">
-        <ChatFill size={25} color="#fff" />
-      </button>
-    </div>
+    <>
+      <div id="floatingButtons">
+        <div className="chatBtn" onClick={toggleChat}>
+          <button
+            className={`chat_onBtn ${isChat ? 'visible' : ''}`}
+            type="button"
+          >
+            <ChatFill size={25} color="#fff" />
+          </button>
+          <button
+            className={`chat_offBtn ${!isChat ? 'visible' : ''}`}
+            type="button"
+          >
+            <X size={35} color="#4f503e" />
+          </button>
+        </div>
+        <button className="topBtn" type="button" onClick={scrollTop}>
+          <ArrowUp size={25} color="#fff" />
+        </button>
+      </div>
+
+      <ChatWindow isChat={isChat} />
+    </>
   );
 }
 
